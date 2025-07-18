ef05cdefde32bdd2d9266074ad97c801bfb31fe8
cho-seunghee <nurcle77@gmail.com>
2025-07-18 10:01:26 +0900

    20250718 수정

--- src/main/java/com/boot/ktn/config/AppConfig.java
+++ src/main/java/com/boot/ktn/config/AppConfig.java
@@ -25,8 +25,8 @@ public class AppConfig {
 
     @Bean
     public FileConfig fileConfig() {
-        long maxFileSize = Long.parseLong(dotenv.get("MAX_FILE_SIZE", "104857600")); // Default to 100MB
-        int maxResultSize = Integer.parseInt(dotenv.get("MAX_RESULT_SIZE", "100")); // Default to 100 records
+        long maxFileSize = Long.parseLong(dotenv.get("MAX_FILE_SIZE", "314572800")); // Default to 300MB
+        int maxResultSize = Integer.parseInt(dotenv.get("MAX_RESULT_SIZE", "300")); // Default to 300 records
         int maxFilesPerUpload = Integer.parseInt(dotenv.get("MAX_FILES_PER_UPLOAD", "10")); // Default to 10 files
         return new FileConfig(maxFileSize, maxResultSize, maxFilesPerUpload);
     }
