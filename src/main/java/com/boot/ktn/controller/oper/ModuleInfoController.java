c19ba92ec047ada7ee68d500aede2d7cec2b908b
cho-seunghee <nurcle77@gmail.com>
2025-08-07 16:53:29 +0900

    20250807 업무정보 controller 추가

---
+++ src/main/java/com/boot/ktn/controller/oper/ModuleInfoController.java
@@ -0,0 +1,71 @@
+package com.boot.ktn.controller.oper;
+
+import com.boot.ktn.dto.common.ApiResponseDto;
+import com.boot.ktn.service.mapview.MapViewProcessor;
+import com.boot.ktn.util.CommonApiResponses;
+import com.boot.ktn.util.EscapeUtil;
+import com.boot.ktn.util.MapViewParamsUtil;
+import com.boot.ktn.util.ResponseEntityUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("${api.base.path}/oper/moduleinfo")
+@RequiredArgsConstructor
+@io.swagger.v3.oas.annotations.tags.Tag(name = "3.시스템관리 > 공통", description = "업무정보를 가져오는 API")
+public class ModuleInfoController {
+    private static final Logger logger = LoggerFactory.getLogger(ModuleInfoController.class);
+
+    private final MapViewProcessor mapViewProcessor;
+    private final ResponseEntityUtil responseEntityUtil;
+    private final EscapeUtil escapeUtil;
+    private final MapViewParamsUtil mapViewParamsUtil;
+
+    @Setter
+    @Getter
+    String errorMessage;
+
+    @CommonApiResponses
+    @PostMapping("/list")
+    public ResponseEntity<ApiResponseDto<List<Map<String, Object>>>> infoList(
+            @RequestBody Map<String, Object> request,
+            HttpServletRequest httpRequest
+    ) {
+        String rptCd = "MODULEINFO";
+        String jobGb = "GET";
+
+        Claims claims = (Claims) httpRequest.getAttribute("user");
+        String empNo = claims != null && claims.getSubject() != null ? claims.getSubject() : null;
+
+        List<String> params = mapViewParamsUtil.getParams(request, escapeUtil);
+
+        List<Map<String, Object>> unescapedResultList;
+        try {
+            unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);
+        } catch (IllegalArgumentException e) {
+            errorMessage = "/list unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);";
+            logger.error(this.getErrorMessage(), e.getMessage(), e);
+            return responseEntityUtil.okBodyEntity(null, "01", e.getMessage());
+        }
+
+        if (unescapedResultList.isEmpty()) {
+            return responseEntityUtil.okBodyEntity(null, "01", "조회 결과가 없습니다.");
+        }
+
+        return responseEntityUtil.okBodyEntity(unescapedResultList);
+    }
+
+}
