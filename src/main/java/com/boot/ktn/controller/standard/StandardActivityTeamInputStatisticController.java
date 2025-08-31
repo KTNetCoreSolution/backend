3d360c7a7da648ec99f2d0a2aa081ace1b3177de
cho-seunghee <nurcle77@gmail.com>
2025-08-31 11:27:15 +0900

    20250831 표준활동 수정

---
+++ src/main/java/com/boot/ktn/controller/standard/StandardActivityTeamInputStatisticController.java
@@ -0,0 +1,76 @@
+package com.boot.ktn.controller.standard;
+
+import com.boot.ktn.config.AppConfig;
+import com.boot.ktn.dto.common.ApiResponseDto;
+import com.boot.ktn.service.mapview.MapViewFileProcessor;
+import com.boot.ktn.service.mapview.MapViewProcessor;
+import com.boot.ktn.util.CommonApiResponses;
+import com.boot.ktn.util.EscapeUtil;
+import com.boot.ktn.util.MapViewParamsUtil;
+import com.boot.ktn.util.ResponseEntityUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("${api.base.path}/standard")
+@RequiredArgsConstructor
+@io.swagger.v3.oas.annotations.tags.Tag(name = "4.표준활동 > 팀별입력현황", description = "팀별입력현황을 관리하는 API")
+public class StandardActivityTeamInputStatisticController {
+    private static final Logger logger = LoggerFactory.getLogger(StandardActivityTeamInputStatisticController.class);
+
+    private final ResponseEntityUtil responseEntityUtil;
+    private final MapViewProcessor mapViewProcessor;
+    private final MapViewFileProcessor mapViewFileProcessor;
+    private final EscapeUtil escapeUtil;
+    private final MapViewParamsUtil mapViewParamsUtil;
+    private final AppConfig.FileConfig fileConfig;
+
+    @Setter
+    @Getter
+    String errorMessage;
+
+    @CommonApiResponses
+    @PostMapping("/teamInputStatistic/list")
+    public ResponseEntity<ApiResponseDto<List<Map<String, Object>>>> teamInputStatisticList(
+            @RequestBody Map<String, Object> request,
+            HttpServletRequest httpRequest
+    ) {
+        String rptCd = "STANDARDACTIVITYTEAMINPUTSTATISTIC";
+        String jobGb = "GET";
+
+        Claims claims = (Claims) httpRequest.getAttribute("user");
+        String empNo = claims != null && claims.getSubject() != null ? claims.getSubject() : null;
+
+        List<String> params = mapViewParamsUtil.getParams(request, escapeUtil);
+
+        List<Map<String, Object>> unescapedResultList;
+        try {
+            unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);
+        } catch (IllegalArgumentException e) {
+            errorMessage = "/list unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);";
+            logger.error(this.getErrorMessage(), e.getMessage(), e);
+            return responseEntityUtil.okBodyEntity(null, "01", e.getMessage());
+        }
+
+        if (unescapedResultList.isEmpty()) {
+            return responseEntityUtil.okBodyEntity(null, "01", "조회 결과가 없습니다.");
+        }
+
+        return responseEntityUtil.okBodyEntity(unescapedResultList);
+    }
+
+
+}
