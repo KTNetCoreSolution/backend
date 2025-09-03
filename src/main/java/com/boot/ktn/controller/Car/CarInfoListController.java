782f2591177268f04052ef9690c3fc9b3287bd3f
BearFl <idle12@kakao.com>
2025-09-03 13:56:44 +0900

    20250903 차량정보관리 양식 변경

--- src/main/java/com/boot/ktn/controller/Car/CarInfoListController.java
+++ src/main/java/com/boot/ktn/controller/Car/CarInfoListController.java
@@ -99,6 +99,36 @@ public class CarInfoListController {
     }
 
     @CommonApiResponses
+    @PostMapping("/rentalCompList")
+    public ResponseEntity<ApiResponseDto<List<Map<String, Object>>>> rentalCompList(
+            @RequestBody Map<String, Object> request,
+            HttpServletRequest httpRequest
+    ) {
+        String rptCd = "RENTALCOMPLIST";
+        String jobGb = "GET";
+
+        Claims claims = (Claims) httpRequest.getAttribute("user");
+        String empNo = claims != null && claims.getSubject() != null ? claims.getSubject() : null;
+
+        List<String> params = mapViewParamsUtil.getParams(request, escapeUtil);
+
+        List<Map<String, Object>> unescapedResultList;
+        try {
+            unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);
+        } catch (IllegalArgumentException e) {
+            errorMessage = "/rentalCompList unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);";
+            logger.error(this.getErrorMessage(), e.getMessage(), e);
+            return responseEntityUtil.okBodyEntity(null, "01", e.getMessage());
+        }
+
+        if (unescapedResultList.isEmpty()) {
+            return responseEntityUtil.okBodyEntity(null, "01", "조회 결과가 없습니다.");
+        }
+
+        return responseEntityUtil.okBodyEntity(unescapedResultList);
+    }
+
+    @CommonApiResponses
     @PostMapping("/CarinfoByCarId")
     public ResponseEntity<ApiResponseDto<List<Map<String, Object>>>> CarinfoByCarId(
             @RequestBody Map<String, Object> request,
