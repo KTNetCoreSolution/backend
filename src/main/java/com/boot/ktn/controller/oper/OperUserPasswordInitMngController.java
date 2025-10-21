9515c798bb8a95206a5d58d35a0f50c38c6d0b38
cho-seunghee <nurcle77@gmail.com>
2025-10-21 10:14:14 +0900

    20251021 비밀번호초기화관리 및 렌탄관련 추가

---
+++ src/main/java/com/boot/ktn/controller/oper/OperUserPasswordInitMngController.java
@@ -0,0 +1,100 @@
+package com.boot.ktn.controller.oper;
+
+import com.boot.ktn.dto.common.ApiResponseDto;
+import com.boot.ktn.service.mapview.MapViewProcessor;
+import com.boot.ktn.util.CommonApiResponses;
+import com.boot.ktn.util.EscapeUtil;
+import com.boot.ktn.util.MapViewParamsUtil;
+import com.boot.ktn.util.ResponseEntityUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("${api.base.path}/oper/userpasswordinitmng")
+@RequiredArgsConstructor
+@io.swagger.v3.oas.annotations.tags.Tag(name = "3.시스템관리 > 사용자비밀번호초기화관리", description = "사용자비밀번호초기화를 관리하는 API")
+public class OperUserPasswordInitMngController {
+    private static final Logger logger = LoggerFactory.getLogger(OperUserPasswordInitMngController.class);
+
+    private final MapViewProcessor mapViewProcessor;
+    private final ResponseEntityUtil responseEntityUtil;
+    private final EscapeUtil escapeUtil;
+    private final MapViewParamsUtil mapViewParamsUtil;
+
+    @Setter
+    @Getter
+    String errorMessage;
+
+    @CommonApiResponses
+    @PostMapping("/list")
+    public ResponseEntity<ApiResponseDto<List<Map<String, Object>>>> userPasswordInitMngList(
+            @RequestBody Map<String, Object> request,
+            HttpServletRequest httpRequest
+    ) {
+        String rptCd = "USERPASSWORDINITMNG";
+        String jobGb = "GET";
+
+        Claims claims = (Claims) httpRequest.getAttribute("user");
+        String empNo = claims != null && claims.getSubject() != null ? claims.getSubject() : null;
+
+        List<String> params = mapViewParamsUtil.getParams(request, escapeUtil);
+
+        List<Map<String, Object>> unescapedResultList;
+        try {
+            unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);
+        } catch (IllegalArgumentException e) {
+            errorMessage = "/list unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);";
+            logger.error(this.getErrorMessage(), e.getMessage(), e);
+            return responseEntityUtil.okBodyEntity(null, "01", e.getMessage());
+        }
+
+        if (unescapedResultList.isEmpty()) {
+            return responseEntityUtil.okBodyEntity(null, "01", "조회 결과가 없습니다.");
+        }
+
+        return responseEntityUtil.okBodyEntity(unescapedResultList);
+    }
+
+    @CommonApiResponses
+    @PostMapping("/save")
+    public ResponseEntity<ApiResponseDto<List<Map<String, Object>>>> userPasswordInitMngSave(
+            @RequestBody Map<String, Object> request,
+            HttpServletRequest httpRequest
+    ) {
+        String rptCd = "USERPASSWORDINITMNGTRAN";
+        String jobGb = "SET";
+
+        Claims claims = (Claims) httpRequest.getAttribute("user");
+        String empNo = claims != null && claims.getSubject() != null ? claims.getSubject() : null;
+
+        List<String> params = mapViewParamsUtil.getParams(request, escapeUtil);
+
+        List<Map<String, Object>> unescapedResultList;
+        try {
+            unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);
+        } catch (IllegalArgumentException e) {
+            errorMessage = "/save unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);";
+            logger.error(this.getErrorMessage(), e.getMessage(), e);
+            return responseEntityUtil.okBodyEntity(null, "01", e.getMessage());
+        }
+
+        if (unescapedResultList.isEmpty()) {
+            return responseEntityUtil.okBodyEntity(null, "01", "결과 없습니다.");
+        }
+
+        return responseEntityUtil.okBodyEntity(unescapedResultList);
+    }
+}
---
+++ src/main/java/com/boot/ktn/controller/rental/RentalBatchMngController.java
@@ -0,0 +1,106 @@
+package com.boot.ktn.controller.rental;
+
+import com.boot.ktn.config.AppConfig;
+import com.boot.ktn.dto.common.ApiResponseDto;
+import com.boot.ktn.service.mapview.MapViewFileProcessor;
+import com.boot.ktn.service.mapview.MapViewProcessor;
+import com.boot.ktn.util.CommonApiResponses;
+import com.boot.ktn.util.EscapeUtil;
+import com.boot.ktn.util.MapViewParamsUtil;
+import com.boot.ktn.util.ResponseEntityUtil;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("${api.base.path}/rental")
+@RequiredArgsConstructor
+@io.swagger.v3.oas.annotations.tags.Tag(name = "5.렌탈관리 > 렌탈일괄관리", description = "렌탈일괄을 관리하는 API")
+public class RentalBatchMngController {
+    private static final Logger logger = LoggerFactory.getLogger(RentalBatchMngController.class);
+
+    private final ResponseEntityUtil responseEntityUtil;
+    private final MapViewProcessor mapViewProcessor;
+    private final MapViewFileProcessor mapViewFileProcessor;
+    private final EscapeUtil escapeUtil;
+    private final MapViewParamsUtil mapViewParamsUtil;
+    private final AppConfig.FileConfig fileConfig;
+
+    @Setter
+    @Getter
+    String errorMessage;
+
+    @CommonApiResponses
+    @PostMapping("/batchMng/list")
+    public ResponseEntity<ApiResponseDto<List<Map<String, Object>>>> batchMngList(
+            @RequestBody Map<String, Object> request,
+            HttpServletRequest httpRequest
+    ) {
+        String rptCd = "RENTALBATCHMNG";
+        String jobGb = "GET";
+
+        Claims claims = (Claims) httpRequest.getAttribute("user");
+        String empNo = claims != null && claims.getSubject() != null ? claims.getSubject() : null;
+
+        List<String> params = mapViewParamsUtil.getParams(request, escapeUtil);
+
+        List<Map<String, Object>> unescapedResultList;
+        try {
+            unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);
+        } catch (IllegalArgumentException e) {
+            errorMessage = "/list unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);";
+            logger.error(this.getErrorMessage(), e.getMessage(), e);
+            return responseEntityUtil.okBodyEntity(null, "01", e.getMessage());
+        }
+
+        if (unescapedResultList.isEmpty()) {
+            return responseEntityUtil.okBodyEntity(null, "01", "조회 결과가 없습니다.");
+        }
+
+        return responseEntityUtil.okBodyEntity(unescapedResultList);
+    }
+
+
+    @CommonApiResponses
+    @PostMapping("/batchMng/save")
+    public ResponseEntity<ApiResponseDto<List<Map<String, Object>>>> batchMngSave(
+            @RequestBody Map<String, Object> request,
+            HttpServletRequest httpRequest
+    ) {
+        String rptCd = "RENTALBATCHMNGTRAN";
+        String jobGb = "SET";
+
+        Claims claims = (Claims) httpRequest.getAttribute("user");
+        String empNo = claims != null && claims.getSubject() != null ? claims.getSubject() : null;
+
+        List<String> params = mapViewParamsUtil.getParams(request, escapeUtil);
+
+        List<Map<String, Object>> unescapedResultList;
+        try {
+            unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);
+        } catch (IllegalArgumentException e) {
+            errorMessage = "/save unescapedResultList = mapViewProcessor.processDynamicView(rptCd, params, empNo, jobGb);";
+            logger.error(this.getErrorMessage(), e.getMessage(), e);
+            return responseEntityUtil.okBodyEntity(null, "01", e.getMessage());
+        }
+
+        if (unescapedResultList.isEmpty()) {
+            return responseEntityUtil.okBodyEntity(null, "01", "결과 없습니다.");
+        }
+
+        return responseEntityUtil.okBodyEntity(unescapedResultList);
+    }
+
+}
